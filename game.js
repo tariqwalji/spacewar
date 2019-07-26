9e7c6741bec054fb130e338279984a0548367eef
Tariq Hussein <tariq.hussein@powwownow.com>
2019-07-26 17:35:17 +0100

    Added angle.

--- game.js
+++ game.js
@@ -1,77 +1,95 @@
-"use strict";
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Star = function () {
-    function Star(x, y, speed, ctx) {
-        _classCallCheck(this, Star);
-
-        this.ctx = ctx;
+class Star {
+    constructor(x, y, speed, world) {
+        this.world = world;
         this.x = x;
         this.y = y;
         this.speed = speed;
-        this.starSize = 5 - Math.round(5 / speed);
+        this.starSize = 5 - Math.round(5/speed);
+        this.angle = world.playerDirection;
     }
 
-    _createClass(Star, [{
-        key: "draw",
-        value: function draw() {
-            var col = 50 * this.speed;
-            this.ctx.fillStyle = "rgb(" + col + ", " + col + ", " + col + ")";
-            this.ctx.fillRect(this.x, this.y, this.starSize, this.starSize);
-            this.x -= this.speed;
-            if (this.x < 0) this.x = window.innerWidth;
-        }
-    }], [{
-        key: "createRandom",
-        value: function createRandom(ctx) {
-            return new Star(Math.ceil(Math.random() * window.innerWidth), Math.ceil(Math.random() * window.innerHeight), Math.ceil(Math.random() * 5), ctx);
-        }
-    }]);
+    draw() {
+        let col = 50 * this.speed;
+        this.world.ctx.fillStyle = `rgb(${col}, ${col}, ${col})`;
+        this.world.ctx.fillRect(this.x, this.y, this.starSize, this.starSize);
+        this.x += Math.sin(this.world.playerDirection) * (this.speed + this.world.playerSpeed);
+        this.y += Math.cos(this.world.playerDirection) * (this.speed + this.world.playerSpeed);
 
-    return Star;
-}();
+        if (this.x < 0) this.x = window.innerWidth;
+        if (this.y < 0) this.y = window.innerHeight;
+        if (this.x > window.innerWidth) this.x = 0;
+        if (this.y > window.innerHeight) this.y = 0;
 
-var Renderer = function () {
-    function Renderer() {
-        _classCallCheck(this, Renderer);
+    }
 
+    static createRandom(world) {
+        return new Star(
+            Math.ceil(Math.random() * window.innerWidth),
+            Math.ceil(Math.random() * window.innerHeight), 
+            Math.ceil(Math.random() * 5),
+            world);
+    }
+}
+
+class World {
+    constructor() {
         this.canvas = document.getElementById("game");
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext("2d");
+        this.playerSpeed = 10;
+        this.playerSpeed = 5;
+        this.playerDirection = Math.PI;
 
+        this.playerImage = new Image(114, 114);
+        this.playerImage.src = "spaceshooter_ByJanaChumi/items/8.png";
+        
         this.stars = [];
-        for (var i = 0; i < 100; i++) {
-            this.stars.push(Star.createRandom(this.ctx));
+        for(let i=0; i<100; i++) {
+            this.stars.push(Star.createRandom(this));
         }
     }
 
-    _createClass(Renderer, [{
-        key: "clearScreen",
-        value: function clearScreen() {
-            this.ctx.fillStyle = "rgb(0, 0, 0)";
-            this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        }
-    }, {
-        key: "draw",
-        value: function draw() {
-            this.clearScreen();
-            this.stars.forEach(function (s) {
-                return s.draw();
-            });
-        }
-    }, {
-        key: "run",
-        value: function run() {
-            this.renderLoop = setInterval(this.draw.bind(this), 50);
-        }
-    }]);
+    clearScreen() {
+        this.ctx.fillStyle = "rgb(0, 0, 0)";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 
-    return Renderer;
-}();
+    draw() {
+        this.clearScreen();
+        this.stars.forEach(s => s.draw());
 
-var r = new Renderer();
-r.run();
+        this.ctx.save();
+        this.ctx.translate(this.canvas.width/2, this.canvas.height/2);
+        this.ctx.rotate((2*Math.PI) * this.playerDirection);
+        this.ctx.drawImage(this.playerImage, this.playerImage.width/2, this.playerImage.height/2);
+        this.ctx.restore();
+    }
+
+    run() {
+        this.renderLoop = setInterval(this.draw.bind(this), 50);
+        document.addEventListener("keydown", e => {
+            switch(e.code) {
+                case "ArrowUp":
+                    this.playerSpeed++;
+                    if(this.playerSpeed > 10) this.playerSpeed = 10;
+                    break;
+                case "ArrowDown":
+                        this.playerSpeed--;
+                        if(this.playerSpeed < 2) this.playerSpeed = 2;
+                        break;
+                case "ArrowLeft":
+                    this.playerDirection += (Math.PI/360)*this.playerSpeed;
+                    if(this.playerDirection > 2*Math.PI) this.playerDirection = 0;
+                    break;
+                case "ArrowRight":
+                    this.playerDirection -= (Math.PI/360)*this.playerSpeed;
+                    if(this.playerDirection < 0) this.playerDirection = 2*Math.PI;
+                    break;
+                }
+        });
+    }
+}
+
+let w = new World();
+w.run();
--- src/game.js
+++ src/game.js
@@ -1,39 +1,52 @@
 class Star {
-    constructor(x, y, speed, ctx) {
-        this.ctx = ctx;
+    constructor(x, y, speed, world) {
+        this.world = world;
         this.x = x;
         this.y = y;
         this.speed = speed;
         this.starSize = 5 - Math.round(5/speed);
+        this.angle = world.playerDirection;
     }
 
     draw() {
         let col = 50 * this.speed;
-        this.ctx.fillStyle = `rgb(${col}, ${col}, ${col})`;
-        this.ctx.fillRect(this.x, this.y, this.starSize, this.starSize);
-        this.x -= this.speed;
+        this.world.ctx.fillStyle = `rgb(${col}, ${col}, ${col})`;
+        this.world.ctx.fillRect(this.x, this.y, this.starSize, this.starSize);
+        this.x += Math.sin(this.world.playerDirection) * (this.speed + this.world.playerSpeed);
+        this.y += Math.cos(this.world.playerDirection) * (this.speed + this.world.playerSpeed);
+
         if (this.x < 0) this.x = window.innerWidth;
+        if (this.y < 0) this.y = window.innerHeight;
+        if (this.x > window.innerWidth) this.x = 0;
+        if (this.y > window.innerHeight) this.y = 0;
+
     }
 
-    static createRandom(ctx) {
+    static createRandom(world) {
         return new Star(
             Math.ceil(Math.random() * window.innerWidth),
             Math.ceil(Math.random() * window.innerHeight), 
             Math.ceil(Math.random() * 5),
-            ctx);
+            world);
     }
 }
 
-class Renderer {
+class World {
     constructor() {
         this.canvas = document.getElementById("game");
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext("2d");
+        this.playerSpeed = 10;
+        this.playerSpeed = 5;
+        this.playerDirection = Math.PI;
+
+        this.playerImage = new Image(114, 114);
+        this.playerImage.src = "spaceshooter_ByJanaChumi/items/8.png";
         
         this.stars = [];
         for(let i=0; i<100; i++) {
-            this.stars.push(Star.createRandom(this.ctx));
+            this.stars.push(Star.createRandom(this));
         }
     }
 
@@ -45,12 +58,38 @@ class Renderer {
     draw() {
         this.clearScreen();
         this.stars.forEach(s => s.draw());
+
+        this.ctx.save();
+        this.ctx.translate(this.canvas.width/2, this.canvas.height/2);
+        this.ctx.rotate((2*Math.PI) * this.playerDirection);
+        this.ctx.drawImage(this.playerImage, this.playerImage.width/2, this.playerImage.height/2);
+        this.ctx.restore();
     }
 
     run() {
         this.renderLoop = setInterval(this.draw.bind(this), 50);
+        document.addEventListener("keydown", e => {
+            switch(e.code) {
+                case "ArrowUp":
+                    this.playerSpeed++;
+                    if(this.playerSpeed > 10) this.playerSpeed = 10;
+                    break;
+                case "ArrowDown":
+                        this.playerSpeed--;
+                        if(this.playerSpeed < 2) this.playerSpeed = 2;
+                        break;
+                case "ArrowLeft":
+                    this.playerDirection += (Math.PI/360)*this.playerSpeed;
+                    if(this.playerDirection > 2*Math.PI) this.playerDirection = 0;
+                    break;
+                case "ArrowRight":
+                    this.playerDirection -= (Math.PI/360)*this.playerSpeed;
+                    if(this.playerDirection < 0) this.playerDirection = 2*Math.PI;
+                    break;
+                }
+        });
     }
 }
 
-let r = new Renderer();
-r.run();
+let w = new World();
+w.run();
